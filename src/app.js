d56b3fd6e3b7ecba1591cb70230152d00915fda8
Fengda HUANG <h@phodal.com>
2015-03-25 09:17:47 +0800

    some ui particles

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ var GameScene = cc.Scene.extend({
 });
 
 var GameLayer = cc.Layer.extend({
+	ui: null,
     ctor:function () {
         this._super();
     },
@@ -20,8 +21,41 @@ var GameLayer = cc.Layer.extend({
 	},
 	initUI: function () {
 		var bg = new cc.Sprite(res.MainBG);
-		bg.anchorX = 0;
-		bg.anchorY = 0;
-		this.addChild(bg, 0);
+		bg.attr({
+			x: 0,
+			y: 0,
+			anchorX: 0,
+			anchorY: 0
+		});
+		this.addChild(bg);
+		//
+		//this.ui = new cc.Sprite(res.ui_bg);
+		//this.ui.x = cc.winSize.width / 2;
+		//this.ui.y = cc.winSize.height / 2;
+		//this.ui.scale = 0;
+		//this.ui.attr({
+		//	x: 100,
+		//	y: 200,
+		//	anchorX: 100,
+		//	anchorY: 200
+		//});
+		//this.addChild(this.ui, 100);
+
+		var title_bg = new cc.Sprite(res.title_bg);
+		this.addChild(title_bg);
+		title_bg.x = this.width / 2;
+		title_bg.y = this.height / 2 + 40;
+
+		var title = new cc.LabelTTF("Phodal", "Arial", 20);
+		title.x = title_bg.width/2;
+		title.y = title_bg.height/2;
+		title_bg.addChild(title);
+
+		//var button = new ccui.Button();
+		//button.setTouchEnabled(true);
+		//button.loadTextures(res.button1, res.button2, "");
+		//this.ui.addChild(button);
+		//button.x = this.ui.width / 2;
+		//button.y = this.ui.height / 2 - 30;
 	}
 });
--- src/resource.js
+++ src/resource.js
@@ -1,6 +1,9 @@
 var res = {
-    HelloWorld_png : "res/HelloWorld.png",
-	MainBG : "res/background.jpg"
+	MainBG : "res/background.jpg",
+	button1: "res/button1.png",
+	button2: "res/button2.png",
+	title_bg: "res/title_bg.png",
+	ui_bg: "res/ui_bg.png"
 };
 
 var g_resources = [];
