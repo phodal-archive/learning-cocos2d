a5f40cf8de4b22a61dcf0b8b4ed76bb9303b4fb9
Fengda HUANG <h@phodal.com>
2015-03-28 22:47:02 +0800

    add tile gid

--- src/layer/BackgroundLayer.js
+++ src/layer/BackgroundLayer.js
@@ -6,6 +6,7 @@ var BackgroundLayer = cc.Layer.extend({
 	space:null,
 	spriteSheet:null,
 	objects:[],
+	obstacle: null,
 
 	ctor:function (space) {
 		this._super();
@@ -13,12 +14,35 @@ var BackgroundLayer = cc.Layer.extend({
 		this.space = space;
 		this.init();
 	},
+	initObstacles : function(map) {
+		this.obstacles = [];
+		var mapWidth = map.getMapSize().width;
+		var mapHeight = map.getMapSize().height;
+		var tileWidth = map.getTileSize().width;
+		var tileHeight = map.getTileSize().height;
+		var collidableLayer = map.getLayer("collidable");
+		var i, j;
+		for (i = 0; i < mapWidth; i++){
+			for (j = 0; j < mapHeight; j++){
+				var tileCoord = new cc.Point(i, j);
+				var gid = collidableLayer.getTileGIDAt(tileCoord);
+				if(gid) {
+					var tileXPositon = i * tileWidth;
+					var tileYPosition = (mapHeight * tileHeight) - ((j+1) * tileHeight);
+					var react = cc.rect(tileXPositon, tileYPosition, tileWidth, tileHeight);
+					this.obstacles.push(react);
+				}
+			}
+		}
+	},
 	init:function () {
 		this.map00 = cc.TMXTiledMap.create(res.map_tmx);
 		var objGroup = this.map00.getObjectGroup('objects');
 		var obj = objGroup.getObject('role');
 		g_runnerStartX = obj.x;
 		g_runnerStartY = obj.y;
+
+		this.initObstacles(this.map00);
 
 		this.addChild(this.map00);
 
